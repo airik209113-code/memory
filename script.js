e46e036d4236e391db8c498d468120230dbd8410
airik209113-code <airik.209113@gmail.com>
2026-06-19 04:54:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const zuege = document.getElementById("moves");
 const neuStart = document.getElementById("restart-btn");
 
 const bilder = [
-    "https://pixabay.com/images/download/gruendercoach-the-sheep-7089786_1920.jpg",
-    "https://pixabay.com/images/download/pexels-red-panda-1851661_1920.jpg",
-    "https://pixabay.com/images/download/kathy_buscher-adler-2386314_1920.jpg",
-    "https://pixabay.com/images/download/pexels-adorable-1852815_1920.jpg",
-    "https://pixabay.com/images/download/11417994-chipmunk-3959206_1920.jpg",
-    "https://pixabay.com/images/download/12019-iceland-1979445_1920.jpg",
-    "https://pixabay.com/images/download/katrinbechtel-dog-7956828_1920.jpg",
-    "https://pixabay.com/images/download/pexels-lemur-1851671_1920.jpg"
+    "https://th.bing.com/th/id/R.0bfdddd3e566064db484e3d951f51920?rik=0ULzHjxatyK9eg&riu=http%3a%2f%2fwww.globalsherpa.org%2fwp-content%2fuploads%2f2011%2f08%2ftokyo-aerial-view-night-skyline.jpg&ehk=2wafmYo%2fF5Mw1iPYogCAAiBrdUciA83%2fzoINEaODkhc%3d&risl=&pid=ImgRaw&r=0",
+    "https://www.worldatlas.com/r/w1200/upload/14/f7/35/shutterstock-259954391.jpg",
+    "https://cdn.worlddata.info/pics/og/megacity-hongkong.jpg",
+    "https://www.bioenergyconsult.com/wp-content/uploads/2022/06/sustainable-megacities.jpg",
+    "https://scwcontent.affino.com/AcuCustom/Sitename/DAM/017/Megacity_AdobeStock_187332287.jpg",
+    "https://tse3.mm.bing.net/th/id/OIP.ZE1CTX3lEi8ccD5Qxk9ZBgHaE7?rs=1&pid=ImgDetMain&o=7&rm=3",
+    "https://media.istockphoto.com/photos/aerial-view-of-mega-city-skyline-picture-id174215815?k=20&m=174215815&s=612x612&w=0&h=rJWrHyAadTZjBusoo3JnuQZ-Yfp0S8mUV-VTrjtczs0=",
+    "https://idsb.tmgrup.com.tr/ly/uploads/images/2022/07/30/221537.jpg"
 ];
 
 let cards = [];
