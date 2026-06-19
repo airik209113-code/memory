2c238e3e2c315c3994d75ea6188bf83c43e7378d
airik209113-code <airik.209113@gmail.com>
2026-06-19 03:34:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const board = document.querySelector(".game-board");
+const movesText = document.getElementById("moves");
+const restartBtn = document.getElementById("restart-btn");
 
+const images = [
+    "images/🫶.jpg",
+    "images/776237685825297858.jpg",
+    "images/234327986859035320.jpg",
+    "images/11047961582700970.jpg",
+    "images/5207355814894851.jpg",
+    "images/1618549864562566.jpg",
+    "images/492649954934903.jpg",
+    "images/422281212092771.jpg"
+];
